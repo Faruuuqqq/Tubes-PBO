8b295507a3c2f423345a4e4699bdcd9ca253736c
Faruuuqqq <faruqmahdison@gmail.com>
2025-11-29 20:31:28 +0700

    tambahin controller logic jenis item & pelanggan

--- LaundryApp/src/controller/JenisItemController.java
+++ LaundryApp/src/controller/JenisItemController.java
@@ -6,8 +6,93 @@ package controller;
 
 /**
  *
- * @author Salsabila
+ * @author Faruq
  */
+
+import config.Koneksi;
+import model.JenisItem;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class JenisItemController {
     
+    private final Connection conn;
+
+    public JenisItemController() {
+        this.conn = Koneksi.getConnection();
+    }
+
+    // 1. READ
+    public List<JenisItem> getAllJenisItem() {
+        List<JenisItem> listJenis = new ArrayList<>();
+        String sql = "SELECT * FROM jenis_item";
+        
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            
+            while (rs.next()) {
+                JenisItem item = new JenisItem();
+                item.setId_jenis_item(rs.getInt("id_jenis_item"));
+                item.setNama_item(rs.getString("nama_item"));
+                item.setHarga_per_kg(rs.getDouble("harga_per_kg"));
+                
+                listJenis.add(item);
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(JenisItemController.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return listJenis;
+    }
+
+    // 2. CREATE
+    public boolean addJenisItem(JenisItem item) {
+        String sql = "INSERT INTO jenis_item (nama_item, harga_per_kg) VALUES (?, ?)";
+        
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, item.getNama_item());
+            ps.setDouble(2, item.getHarga_per_kg());
+            
+            return ps.executeUpdate() > 0;
+        } catch (SQLException e) {
+            Logger.getLogger(JenisItemController.class.getName()).log(Level.SEVERE, null, e);
+            return false;
+        }
+    }
+
+    // 3. UPDATE
+    public boolean updateJenisItem(JenisItem item) {
+        String sql = "UPDATE jenis_item SET nama_item=?, harga_per_kg=? WHERE id_jenis_item=?";
+        
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, item.getNama_item());
+            ps.setDouble(2, item.getHarga_per_kg());
+            ps.setInt(3, item.getId_jenis_item());
+            
+            return ps.executeUpdate() > 0;
+        } catch (SQLException e) {
+            Logger.getLogger(JenisItemController.class.getName()).log(Level.SEVERE, null, e);
+            return false;
+        }
+    }
+
+    // 4. DELETE
+    public boolean deleteJenisItem(int id) {
+        String sql = "DELETE FROM jenis_item WHERE id_jenis_item=?";
+        
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+            
+            return ps.executeUpdate() > 0;
+        } catch (SQLException e) {
+            Logger.getLogger(JenisItemController.class.getName()).log(Level.SEVERE, null, e);
+            return false;
+        }
+    }
 }
--- LaundryApp/src/controller/PelangganController.java
+++ LaundryApp/src/controller/PelangganController.java
@@ -6,8 +6,96 @@ package controller;
 
 /**
  *
- * @author Salsabila
+ * @author Faruq
  */
+
+import config.Koneksi;
+import model.Pelanggan;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class PelangganController {
     
+    private final Connection conn;
+
+    public PelangganController() {
+        this.conn = Koneksi.getConnection();
+    }
+
+    // 1. READ (Ambil semua data pelanggan)
+    public List<Pelanggan> getAllPelanggan() {
+        List<Pelanggan> listPelanggan = new ArrayList<>();
+        String sql = "SELECT * FROM pelanggan";
+        
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            
+            while (rs.next()) {
+                Pelanggan p = new Pelanggan();
+                p.setId_pelanggan(rs.getInt("id_pelanggan"));
+                p.setNama_pelanggan(rs.getString("nama_pelanggan"));
+                p.setNo_hp_pelanggan(rs.getString("no_hp_pelanggan"));
+                p.setAlamat_pelanggan(rs.getString("alamat_pelanggan"));
+                
+                listPelanggan.add(p);
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(PelangganController.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return listPelanggan;
+    }
+
+    // 2. CREATE (Tambah pelanggan)
+    public boolean addPelanggan(Pelanggan p) {
+        String sql = "INSERT INTO pelanggan (nama_pelanggan, no_hp_pelanggan, alamat_pelanggan) VALUES (?, ?, ?)";
+        
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, p.getNama_pelanggan());
+            ps.setString(2, p.getNo_hp_pelanggan());
+            ps.setString(3, p.getAlamat_pelanggan());
+            
+            return ps.executeUpdate() > 0;
+        } catch (SQLException e) {
+            Logger.getLogger(PelangganController.class.getName()).log(Level.SEVERE, null, e);
+            return false;
+        }
+    }
+
+    // 3. UPDATE (Edit pelanggan)
+    public boolean updatePelanggan(Pelanggan p) {
+        String sql = "UPDATE pelanggan SET nama_pelanggan=?, no_hp_pelanggan=?, alamat_pelanggan=? WHERE id_pelanggan=?";
+        
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, p.getNama_pelanggan());
+            ps.setString(2, p.getNo_hp_pelanggan());
+            ps.setString(3, p.getAlamat_pelanggan());
+            ps.setInt(4, p.getId_pelanggan());
+            
+            return ps.executeUpdate() > 0;
+        } catch (SQLException e) {
+            Logger.getLogger(PelangganController.class.getName()).log(Level.SEVERE, null, e);
+            return false;
+        }
+    }
+
+    // 4. DELETE (Hapus pelanggan)
+    public boolean deletePelanggan(int id) {
+        String sql = "DELETE FROM pelanggan WHERE id_pelanggan=?";
+        
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+            
+            return ps.executeUpdate() > 0;
+        } catch (SQLException e) {
+            Logger.getLogger(PelangganController.class.getName()).log(Level.SEVERE, null, e);
+            return false;
+        }
+    }
 }
